6aee5afe3cb51ab65e304dfcf00343441d571ded
Muhwezi <intricatesyllable@gmail.com>
2023-12-15 01:21:05 +0300

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line,

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 **/
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char str[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
 write(STDOUT_FILENO, str, sizeof(str));
 write(STDOUT_FILENO, "\n", 1);
 return (1);
