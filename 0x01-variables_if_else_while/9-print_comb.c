7b399f54846ab0e05881bc570ae886fca1c1d863
Muhwezi <intricatesyllable@gmail.com>
2023-12-18 13:32:12 +0300

    arithmetic operators and how to use them

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,17 +5,16 @@
 **/
 int main(void)
 {
-int numbr = 48, underscore = 32, comma = 44;
+int numbr = 48, space = 32, comma = 44;
 while (numbr <= 57)
 {
 putchar(numbr);
-putchar(comma);
-putchar(underscore);
-++numbr;
-if (numbr == 57)
+if (numbr != 57)
 {
-comma = comma - 44;
+putchar(comma);
+putchar(space);
 }
+numbr++;
 }
 putchar(10);
 return (0);
