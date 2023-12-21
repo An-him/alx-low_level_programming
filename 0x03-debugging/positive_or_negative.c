deddf6665313665739e69013abfb72acdc0ccf56
Muhwezi <intricatesyllable@gmail.com>
2023-12-21 11:20:00 +0300

    Debugging a function I did earlier checking if negative or postive or zero and prints the result

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*
+**/
+
+void positive_or_negative(int i)
+{
+if (i > 0)
+	{
+	printf("is positive\n");
+	}
+	else if (i < 0)
+	{
+	printf("is negative\n");
+	}
+	else if (i == 0)
+	{
+	printf("is zero\n");
+}	
+}
