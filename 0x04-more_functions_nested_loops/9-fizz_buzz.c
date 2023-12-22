cca807794f33b7f9a71096a825cc546eb80af1dc
Muhwezi <intricatesyllable@gmail.com>
2023-12-22 04:50:32 +0300

    The “Fizz-Buzz test”

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,12 @@ int main(void)
 int i = 1;
 for (; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 5 == 0 && i % 3 == 0)
+{
+printf("FizzBuzz ");
+continue;
+}
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 continue;
@@ -16,11 +21,6 @@ continue;
 else if (i % 5 == 0)
 {
 printf("Buzz ");
-continue;
-}
-else if (i % 5 == 0 || i % 3 == 0)
-{
-printf("FizzBuzz");
 continue;
 }
 printf("%i ", i);
