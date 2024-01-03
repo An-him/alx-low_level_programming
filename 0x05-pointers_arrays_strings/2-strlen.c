d1ead57d1d8c7303850329db58929dcbc7cfd93e
Muhwezi <intricatesyllable@gmail.com>
2024-01-04 01:33:15 +0300

    function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strlen - function that returns the length of a string
+*@s: Number to swap
+*Return: Length of string
+**/
+int _strlen(char *s)
+{
+int length = 0;
+for (length = 0; length[s] != '\0'; length++)
+{
+length++;
+}
+return (length);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,4 +1,7 @@
 0x05-pointers_arrays_strings
 0-reset_to_98.c
 	function that takes a pointer to an int as parameter and updates the value it points to to 98.
+	dereferncing only.
+2-strlen.c
+	function that returns the length of a string
 
