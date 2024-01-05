b46d98357fd12197627babb1f7125ef921ab315f
Muhwezi <intricatesyllable@gmail.com>
2024-01-05 23:51:17 +0300

    Function that copies a string.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strncpy - function that copies two strings
+*@dest: pointer to destination
+*@src: source to destination
+*@n: bytes from src
+*Return: pointer to char
+**/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (i = 0; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
+}
