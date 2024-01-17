be06c6f40b3756ccaf00757320906aa46c86fa54
Muhwezi <intricatesyllable@gmail.com>
2024-01-18 01:41:03 +0300

    unction that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_strdup - pointer to mem with string duplicated
+*@str: size of bytes to allocate
+*Return: pointer to allocated memory
+**/
+char *_strdup(char *str)
+{
+int i;
+char *strc = NULL;
+int j = strlen(str);
+strc = (char *)malloc(sizeof(char) * j);
+if (strc == NULL)
+{
+return (NULL);
+}
+while (str[i] != '\0')
+{
+strc[i] = str[i];
+i++;
+}
+return (strc);
+}
