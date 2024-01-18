70e0be3663d5b56351defb1c8e4bcfd6c58ba6b5
Muhwezi <intricatesyllable@gmail.com>
2024-01-18 05:28:08 +0300

    unction that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,21 +6,5 @@
 **/
 char *_strdup(char *str)
 {
-int i, j;
-char *strc = NULL;
-if (str == NULL)
-{
-return (NULL);
-}
-j = strlen(str);
-strc = malloc((j + 1) * sizeof(char));
-if (strc == NULL)
-{
-return (NULL);
-}
-for (i = 0; i <= j; i++)
-{
-strc[i] = str[i];
-}
 return (strc);
 }
