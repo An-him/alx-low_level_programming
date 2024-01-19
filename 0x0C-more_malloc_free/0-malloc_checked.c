bbec201806f595d4f8190fa3efc0cd4603b9677c
Muhwezi <intricatesyllable@gmail.com>
2024-01-19 22:06:43 +0300

    function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*malloc_checked - function that allocates memory using malloc
+*@b: size
+*Return: void
+**/
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(sizeof(void *) * b);
+if (ptr == NULL)
+{
+exit(98);
+}
+else
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,5 @@
+0x0C-more_malloc_free
+0-malloc_checked.c
+First number took me some time to come around as I recently thought all void datatypes never return
+	But I got the hang of it.
+
