75644e7ee1f6fb88c5d33a8d06bd696725589aca
Muhwezi <intricatesyllable@gmail.com>
2024-01-24 14:19:34 +0300

    a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - stores dog.h data type definition
+ * @name: dog's name
+ * @age: dog's age in years
+ * @owner: owner of dog
+ * Description: stores dog's Identification fields
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
