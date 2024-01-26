865bd2d4e042767989b8c176b8d8f8709854e41b
Muhwezi <intricatesyllable@gmail.com>
2024-01-26 17:25:13 +0300

    prints numbers, followed by a new line.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int _putchar(char c);
 
 #endif
