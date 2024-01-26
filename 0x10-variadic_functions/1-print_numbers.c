757e31815d4df60c746fa8dccff8e54638ebe015
Muhwezi <intricatesyllable@gmail.com>
2024-01-26 17:24:43 +0300

    prints numbers, followed by a new line.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+*print_numbers - prints numbers, followed by a new line
+*@separator: to print before string
+*@n: number of variables
+*Return: void
+**/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+int x;
+va_list ap;
+va_start(ap, n);
+
+for (i = 0; i < n; i++)
+{
+x = va_arg(ap, int);
+printf("%i", x);
+if (separator == NULL)
+{
+return;
+}
+if (i != n - 1)
+printf("%s", separator);
+}
+putchar('\n');
+va_end(ap);
+}
