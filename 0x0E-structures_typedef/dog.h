f41136cefc14919a4069dfb522a11d059770607f
Muhwezi <michealphenh10@gmail.com>
2024-02-04 02:51:18 +0300

    Added Null handler to this function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct dog - stores dog.h data type definition
  * @name: dog's name
@@ -8,13 +9,15 @@
  * @owner: owner of dog
  * Description: stores dog's Identification fields
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}
+dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
