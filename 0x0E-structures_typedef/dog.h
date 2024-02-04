8718a5386f9538aed1cef231b385ed2310a17067
Muhwezi <michealphenh10@gmail.com>
2024-02-04 04:15:14 +0300

    Prints contents while looking for ways of iterating my Spirit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dog - stores dog.h data type definition
  * @name: dog's name
@@ -18,6 +19,7 @@ char *owner;
 dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
