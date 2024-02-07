8b52167fadd6b46663ae33045d5b7e3cd94a8bee
Muhwezi <michealphenh10@gmail.com>
2024-02-07 04:43:39 +0300

    Add node at the end of a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,18 @@
 #include "lists.h"
 /**
 *free_listint2 - function removes a mode
-*@head: head of l
-*
-*
+*@head: head of list
+*Return: void
 *
 **/
 void free_listint2(listint_t **head)
 {
 listint_t *current = *head;
 listint_t *trav;
+if (*head == NULL)
+{
+return;
+}
 while (current != NULL)
 {
 trav = current->next;
