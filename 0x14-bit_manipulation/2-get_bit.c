5e51d294d0b9f8d3636e951915c67912040bb167
Muhwezi <michealphenh10@gmail.com>
2024-02-09 03:50:15 +0300

    Enter the dragon

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*get_bit - gets bit at index
+*@n: number to shift
+*@index: index of number to shift
+*Return: int
+**/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+return (n = (n & (1UL << index)) >>index);
+}
