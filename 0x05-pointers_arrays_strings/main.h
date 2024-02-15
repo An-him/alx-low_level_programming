22fb715d8187bcbde6c4c1d1c6be75f0cd6bf471
Muhwezi <michealphenh10@gmail.com>
2024-02-15 04:07:01 +0300

    function that reverses a string.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,4 +18,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
+
 #endif
