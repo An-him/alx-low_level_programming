ec16b4ba2a20cad73fe057e73265410a0fea78b4
Muhwezi <michealphenh10@gmail.com>
2024-02-17 01:37:33 +0300

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
-*create_file - creates file
-*@filename: file to be created
-*@text_content: To fill in the filename
-*Return: int
-**/
+ * create_file - creates file
+ * @filename: file to be created
+ * @text_content: content to be written to the file
+ * Return: 1 on success, -1 on failure
+ **/
 int create_file(const char *filename, char *text_content)
 {
-int fp;
-ssize_t CharsRead;
+int fp, i;
+ssize_t CharsWritten;
 
-CharsRead = 0;
 if (filename == NULL)
 {
 return (-1);
 }
 
-fp = open(filename, O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fp == -1)
 {
 return (-1);
@@ -24,8 +23,11 @@ return (-1);
 
 if (text_content != NULL)
 {
-CharsRead = write(fp, text_content, strlen(text_content));
-if (CharsRead == -1)
+for (i = 0; text_content[i]; i++)
+{
+}
+CharsWritten = write(fp, text_content, i);
+if (CharsWritten == -1)
 {
 close(fp);
 return (-1);
