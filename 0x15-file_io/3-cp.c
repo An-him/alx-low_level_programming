25aef3d39bdd0f2974f50922b6d17e9256ed59f5
Muhwezi <michealphenh10@gmail.com>
2024-02-17 04:36:29 +0300

    Compile failure fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,18 @@
-
 #include "main.h"
+/**
+*error_exit - checks for error code
+*@code: code to check for
+*@format: format to check for
+*Return: int
+**/
+void error_exit(int code, const char *format, ...)
+{
+va_list args;
+va_start(args, format);
+dprintf(STDERR_FILENO, format, args);
+va_end(args);
+exit(code);
+}
 /**
 *main - copies file to file
 *@argc: count
