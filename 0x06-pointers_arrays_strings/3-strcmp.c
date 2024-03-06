fc4b8ce19a6975a15065071daf2010ad2ebd55fb
Muhwezi <michealphenh10@gmail.com>
2024-03-06 23:19:54 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,24 +7,14 @@
 **/
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int j;
-	int nl;
-
-	nl = 0;
-
-	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; ++i, ++j)
-	{
-		if (s1[i] == s2[j])
-		{
-			nl = 0;
-		}
-		else if (s1[i] < s2[j])
-		{
-			nl = s1[i] - s2[j];
-		}
-		else
-			nl = s1[i] - s2[j];
-	}
-	return (nl);
+int i;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[1])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (s1[i] - s2[i]);
 }
