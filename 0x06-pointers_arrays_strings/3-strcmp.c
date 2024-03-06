0c6132cedde1a42eb3083284c6bede3e634a2b67
Muhwezi <michealphenh10@gmail.com>
2024-03-06 22:58:00 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,32 +1,30 @@
 #include "main.h"
 /**
-*
-*
-*
-*
+*_strcmp - function compares two strings.
+*@s1: string 1
+*@s2: string 2
+*Return: int
 **/
 int _strcmp(char *s1, char *s2)
 {
-int lengthfors1, lengthfors2, strcmp;
-while (s1[lengthfors1] != '\0' && s2[lengthfors2] != '\0')
-{
-lengthfors1++;
-lengthfors2++;
-}
-if (lengthfors1 == lengthfors2)
-{
-strcmp = 0;
-return (strcmp);
-}
-else if (lengthfors1 > lengthfors2)
-{
-strcmp--;
-return (strcmp);
-}
-else
-{
-strcmp++;
-return (strcmp);
-}
-return (strcmp);
+	int i;
+	int j;
+	int nl;
+
+	nl = 0;
+
+	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; ++i, ++j)
+	{
+		if (s1[i] == s2[j])
+		{
+			nl = 0;
+		}
+		else if (s1[i] < s2[j])
+		{
+			nl = s1[i] - s2[j];
+		}
+		else
+			nl = s1[i] - s2[j];
+	}
+	return (nl);
 }
