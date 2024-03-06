3ac0222359f141c3ccfef3abe066783228e2a1a8
Muhwezi <michealphenh10@gmail.com>
2024-03-06 23:23:40 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[1])
 {
-return (s1[i] - s2[i]);
+return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 i++;
 }
-return (s1[i] - s2[i]);
+return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
