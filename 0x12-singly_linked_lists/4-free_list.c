6d6e580e9cb8d9557b2c53536a7f34f8dc336cc2
Muhwezi <michealphenh10@gmail.com>
2024-03-24 01:57:13 +0300

    Frees list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,5 +6,15 @@
 **/
 void free_list(list_t *head)
 {
-free(head);
+	list_t *temp = NULL;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+		temp = head;
+	}
+
 }
