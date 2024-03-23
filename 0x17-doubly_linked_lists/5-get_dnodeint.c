fdd2ee943b51a9020743f340993b38c0385da775
Muhwezi <michealphenh10@gmail.com>
2024-03-23 04:11:05 +0300

    Finds node @ N

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,8 +9,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
 	unsigned int iter;
-	
+
 	iter = 0;
+
+	if (head == NULL || current == NULL)
+	{
+		return (NULL);
+	}
+
 	while (current != NULL)
 	{
 		if (iter == index)
