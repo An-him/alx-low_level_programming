4ba9e4bd63d6d36a70cf13b0fd8c788bc8227886
Muhwezi <michealphenh10@gmail.com>
2024-04-12 23:16:21 +0300

    README.md, Header and Fx for creating hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - creates hash tables
+ *@size: size of the table to create
+ *Return: allocated space for hash_table
+ *
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+
+	new = malloc(sizeof(hash_table_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->size = size;
+	return (new);
+}
